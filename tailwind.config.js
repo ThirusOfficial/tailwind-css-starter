0c7e587d27a040a13abf575aa6c4c18c696d2aa2
Shruti Balasa <thirusofficial@gmail.com>
2021-01-05 00:21:20 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './public/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
